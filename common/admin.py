650f354be0d9feccd8c366b048a2c8c9daee0520
Fernando Rorato <fernando.rorato@gmail.com>
2025-07-30 13:53:35 -0300

    UPDATE BD

--- common/admin.py
+++ common/admin.py
@@ -1,4 +1,4 @@
-from common.models import Country, State, City
+from common.models import Country, State, City, Origin
 from django.contrib import admin
 
 
@@ -20,4 +20,10 @@ class CityAdmin(admin.ModelAdmin):
     list_display = ('name', 'state', 'created_at', 'updated_at')
     search_fields = ('name',)
     list_filter = ('state',)
+    ordering = ('name',)
+
+@admin.register(Origin)
+class OriginAdmin(admin.ModelAdmin):
+    list_display = ('name',  'created_at', 'updated_at')
+    search_fields = ('name',)
     ordering = ('name',)
