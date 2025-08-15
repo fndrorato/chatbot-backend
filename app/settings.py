995284a41bbed94445a84abb8a10b92740215fee
Fernando Rorato <fernando.rorato@gmail.com>
2025-08-15 17:47:20 -0300

    BD persistente

--- app/settings.py
+++ app/settings.py
@@ -85,7 +85,7 @@ if ENV == 'dev':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': BASE_DIR / 'data' / 'db.sqlite3',
         }
     }
 else:
