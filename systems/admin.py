19c8a1dc0ea1c2ade3ad5c3b4ea7faf4cbbca12f
Fernando Rorato <fernando.rorato@gmail.com>
2025-08-18 11:07:03 -0300

    Admin do Systems

--- systems/admin.py
+++ systems/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from systems.models import LogIntegration, HotelRooms
 
-# Register your models here.
+@admin.register(LogIntegration)
+class LogIntegrationAdmin(admin.ModelAdmin):
+    list_display = ('client_id', 'origin', 'to', 'status_http', 'created_at')
+    search_fields = ('client_id__name', 'origin', 'to')
+    list_filter = ('status_http', 'created_at')
+    ordering = ('-created_at',)
+
+@admin.register(HotelRooms)
+class HotelRoomsAdmin(admin.ModelAdmin):
+    list_display = ('client_id', 'room_code', 'room_type', 'created_at')
+    search_fields = ('client_id__name', 'room_code', 'room_type')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
+
