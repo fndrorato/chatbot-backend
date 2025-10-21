93f4d35db9da52582ed651583b91df841da27f4b
Fernando Rorato <fernando.rorato@gmail.com>
2025-10-21 16:40:04 -0300

    fixed urls.py

--- clients/urls.py
+++ clients/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from clients.views import GetClientBasicInfoView
 
+
+app_name = 'clients'
+
 urlpatterns = [
     path('info/', GetClientBasicInfoView.as_view(), name='get-client-info'),
 ]
